e0cf6d9dd8e549ae692b1201d44cdf836069950c
João Castro <castrovoip@gmail.com>
2025-04-03 19:16:32 -0300

    Feat: Add login component screen

--- gitapps/src/Components/LoginComponent/index.tsx
+++ gitapps/src/Components/LoginComponent/index.tsx
@@ -1,10 +1,36 @@
-import { FormContainer, LoginContainer, LogoContainer } from "./styles";
+import {
+  CheckBoxContainer,
+  FormContainer,
+  LabelContainer,
+  LoginContainer,
+  LogoContainer,
+} from "./styles";
+import LOGINLOGO from "../../assets/LOGINLOGO.svg";
+//import { useForm } from "react-hook-form";
 
 function Login() {
+  //const { register, handleSubmit } = useForm();
+
   return (
     <LoginContainer>
-      <LogoContainer>a</LogoContainer>
-      <FormContainer>a</FormContainer>
+      <LogoContainer>
+        <img src={LOGINLOGO} />
+        <p>software</p>
+      </LogoContainer>
+      <FormContainer id="FormContainer 001">
+        <LabelContainer>
+          <label htmlFor="user_id">Login</label>
+        </LabelContainer>
+        <input type="text" id="user_id" />
+        <LabelContainer>
+          <label htmlFor="user_password">Senha</label>
+        </LabelContainer>
+        <input type="password" />
+        <CheckBoxContainer>
+          <input type="checkbox" />
+          Me manter conectado
+        </CheckBoxContainer>
+      </FormContainer>
     </LoginContainer>
   );
 }
--- gitapps/src/Components/LoginComponent/styles.ts
+++ gitapps/src/Components/LoginComponent/styles.ts
@@ -4,18 +4,65 @@ export const LoginContainer = styled.div`
     width: 500px;
     border:1px solid var(--light-blue);
     border-radius: 10px;
-    max-height: 800px;
-    margin: 150px auto;
+    height: 700px;
+    margin: 100px auto;
     display: grid;
-    grid-template-rows: 50%;
+    grid-template-rows: 40% 60% 20%;
 `
 export const FormContainer = styled.div`
-    width: 100%;
-    background-color: red;
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    padding: 0 50px 0 50px;
+    input{
+        height: 40px;
+        background-color: var(--darker-blue);
+        border: 1px solid var(--light-blue);
+        border-radius: 8px;
+        padding: 0 0 0 20px;
+        font-size: 16px;
+    }    
+    label{
+        align-items: start;
+    }
 `;
-export const LogoContainer = styled.div`
-    width: 100%;
 
-    color: black;
+export const LabelContainer = styled.div`
+    display: flex;
+    justify-content: start;
+    padding: 0 0 0 20px;
+    margin: 10px 0 10px 0;
+    font-size: 20px;
+`;
+
+export const CheckBoxContainer = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    padding-left:20px;
+`;
+
+export const LogoContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    img{
+        height: 140px;
+        width: auto;
+    }
+    p{
+        font-family: 'Montserrat', sans-serif;
+        letter-spacing: 6px;
+        font-size: 30px;
+        color: white;
+        margin: 0;
+    }
+    input{
+        width: 100%;
+        height: 20px;
+    }
+    
 `;
 

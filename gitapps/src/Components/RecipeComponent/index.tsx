34058a2a018e5cc207c4edd769204fe4e5903136
jpcastro14 <castrovoip@gmail.com>
2025-07-22 18:46:38 -0300

    Feat Add form validation logic

--- gitapps/src/Components/RecipeComponent/index.tsx
+++ gitapps/src/Components/RecipeComponent/index.tsx
@@ -1,26 +1,57 @@
-import { FormLabel, Grid2, Slider, TextField, } from "@mui/material"
+import { Button, FormLabel, Grid2, Slider, TextField, } from "@mui/material"
 import { CheckItem, Container } from "./styles"
+import { useForm } from "react-hook-form"
+import { useState } from "react"
+
+interface IRecipe {
+    name: string;
+    prepareTime: string;
+    dificulty: string;
+    ingredients: string;
+    prepareSteps: string
+
+}
 
 function RecipeApp() {
 
-    return (<Container >
+    const [newRecipe, setNewRecipe] = useState<IRecipe>({} as IRecipe)
 
+
+    const { register, reset, getValues, handleSubmit, formState: { errors } } = useForm<IRecipe>()
+
+    const logRecipe = (d: IRecipe) => {
+
+        setNewRecipe(d)
+
+        console.log(newRecipe);
+
+    }
+
+
+    return (<Container >
         <Grid2 container spacing={1} columns={{ xs: 1, md: 4 }}  >
-            <Grid2 size={2} container  >
+            <Grid2 size={2.5} container  >
                 <FormLabel>Nome da receita</FormLabel>
-                <TextField id="recipeName" fullWidth variant="outlined" size="small" ></TextField >
+                <TextField fullWidth variant="outlined" size="small" {...register('name', { required: true })} />
             </Grid2>
-            <Grid2 size={1} container  >
+            <Grid2 size={0.5} container  >
                 <FormLabel >Preparo</FormLabel>
-                <TextField id="recipeName" type="text" fullWidth variant="outlined" size="small" />
+                <TextField type="text" fullWidth variant="outlined" size="small" {...register("prepareTime", { required: true })} />
             </Grid2>
             <Grid2 size={1} container >
                 <FormLabel >Dificuldade</FormLabel>
-                <Slider id="dificulty" size="small" defaultValue={0} max={5} step={1} valueLabelDisplay="on" />
+                <Slider defaultValue={0} min={0} max={5} valueLabelDisplay="on"  {...register("dificulty")} />
+            </Grid2>
+            <Grid2 size={4} container >
+                <FormLabel >Ingredientes</FormLabel>
+                <TextField multiline type="text" fullWidth minRows={2} maxRows={4} size="medium" {...register("ingredients")} />
             </Grid2>
             <Grid2 size={4} container >
                 <FormLabel >Como preparar</FormLabel>
-                <TextField id="dificulty" multiline type="text" fullWidth minRows={2} maxRows={4} size="medium" />
+                <TextField multiline type="text" fullWidth minRows={2} maxRows={4} size="medium" {...register("prepareSteps")} />
+            </Grid2>
+            <Grid2 size={4} container >
+                <Button fullWidth color="primary" variant="contained" onClick={() => handleSubmit(logRecipe)()}>Criar</Button>
             </Grid2>
 
         </Grid2>

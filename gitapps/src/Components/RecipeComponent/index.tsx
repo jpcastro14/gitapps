05a57051ce693efc7dc23355d3a5c62b32332aef
jpcastro14 <castrovoip@gmail.com>
2025-07-23 07:31:22 -0300

    Feat: Add new styled homepage

--- gitapps/src/Components/RecipeComponent/index.tsx
+++ gitapps/src/Components/RecipeComponent/index.tsx
@@ -1,64 +1,25 @@
-import { Button, FormLabel, Grid2, Slider, TextField, } from "@mui/material"
-import { CheckItem, Container } from "./styles"
-import { useForm } from "react-hook-form"
-import { useState } from "react"
+import { BottomSection, Container, MidSection, TopNav } from "./styles";
 
-interface IRecipe {
-    name: string;
-    prepareTime: string;
-    dificulty: string;
-    ingredients: string;
-    prepareSteps: string
-
-}
 
 function RecipeApp() {
-
-    const [newRecipe, setNewRecipe] = useState<IRecipe>({} as IRecipe)
-
-
-    const { register, reset, getValues, handleSubmit, formState: { errors } } = useForm<IRecipe>()
-
-    const logRecipe = (d: IRecipe) => {
-
-        setNewRecipe(d)
-
-        console.log(newRecipe);
-
-    }
-
-
-    return (<Container >
-        <Grid2 container spacing={1} columns={{ xs: 1, md: 4 }}  >
-            <Grid2 size={2.5} container  >
-                <FormLabel>Nome da receita</FormLabel>
-                <TextField fullWidth variant="outlined" size="small" {...register('name', { required: true })} />
-            </Grid2>
-            <Grid2 size={0.5} container  >
-                <FormLabel >Preparo</FormLabel>
-                <TextField type="text" fullWidth variant="outlined" size="small" {...register("prepareTime", { required: true })} />
-            </Grid2>
-            <Grid2 size={1} container >
-                <FormLabel >Dificuldade</FormLabel>
-                <Slider defaultValue={0} min={0} max={5} valueLabelDisplay="on"  {...register("dificulty")} />
-            </Grid2>
-            <Grid2 size={4} container >
-                <FormLabel >Ingredientes</FormLabel>
-                <TextField multiline type="text" fullWidth minRows={2} maxRows={4} size="medium" {...register("ingredients")} />
-            </Grid2>
-            <Grid2 size={4} container >
-                <FormLabel >Como preparar</FormLabel>
-                <TextField multiline type="text" fullWidth minRows={2} maxRows={4} size="medium" {...register("prepareSteps")} />
-            </Grid2>
-            <Grid2 size={4} container >
-                <Button fullWidth color="primary" variant="contained" onClick={() => handleSubmit(logRecipe)()}>Criar</Button>
-            </Grid2>
-
-        </Grid2>
-
-
-
-    </Container>)
+    return (
+        <Container>
+            <TopNav>
+                <p> Livrinho de <span>receitas</span></p>
+                <button>Criar Receita</button>
+            </TopNav>
+            <MidSection>
+                <p>Encontrar receitas</p>
+                <input type="text" />
+                <span>Centenas de receitas, seja para convidar os amigos para um jantar, ou agilizar a semana com pratos práticos e econômicos.</span>
+            </MidSection>
+            <BottomSection>
+                <p>
+                    Desenvolvido por João Castro
+                </p>
+            </BottomSection>
+        </Container>
+    )
 }
 
 export default RecipeApp
--- gitapps/src/Components/RecipeComponent/styles.ts
+++ gitapps/src/Components/RecipeComponent/styles.ts
@@ -1,15 +1,85 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  background-color: aliceblue;
-  padding: 20px;
   width: 100%;
-  div {
-    color: red;
+  background-color: #f3f3f3;
+  display: grid;
+  grid-template-rows: 20% 70% 10%;
+  grid-template-columns: 100%;
+`;
+
+export const TopNav = styled.div`
+  height: 40px;
+  display: flex;
+  font-size: 25px;
+  font-weight: 500;
+  font-family: Poppins;
+  align-items: stretch;
+  padding: 50px 80px 0 80px;
+  justify-content: space-between;
+  p {
+    margin: 0;
+    padding: 0;
+    color: var(--form-black);
+  }
+  span {
+    color: var(--form-red);
+  }
+  button {
+    height: 35px;
+    border: none;
+    font-weight: 500;
+    border-radius: 6px;
+    font-family: Poppins;
+    color: var(--form-black);
+    background-color: var(--form-yellow);
   }
 `;
 
-export const CheckItem = styled.div`
-  background-color: red;
-  width: 100%;
+export const MidSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 0 100px 0 100px;
+  margin-top: 150px;
+  font-family: poppins;
+  align-items: center;
+  p {
+    color: var(--form-red);
+  }
+  span {
+    width: 400px;
+    margin-top: 20px;
+    font-size: smaller;
+    color: var(--form-red);
+  }
+  input {
+    width: 80%;
+    background-color: aliceblue;
+    border-radius: 50px;
+    border: none;
+    height: 40px;
+    box-shadow: 0px 10px 19px -7px rgba(0, 0, 0, 0.75);
+    -webkit-box-shadow: 0px 10px 19px -7px rgba(0, 0, 0, 0.75);
+    -moz-box-shadow: 0px 10px 19px -7px rgba(0, 0, 0, 0.75);
+    color: var(--form-black);
+    padding: 0 20px 0 20px;
+    max-width: 500px;
+  }
+`;
+
+export const BottomSection = styled.div`
+  background-color: var(--form-red);
+  height: 40px;
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  left: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  p {
+    font-family: poppins;
+    font-size: 10px;
+    font-weight: 200;
+  }
 `;

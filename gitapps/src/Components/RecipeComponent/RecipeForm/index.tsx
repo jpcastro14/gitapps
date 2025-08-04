2df47c32663f6b86f18aa1fabc42bebeec52266b
João Castro <castrovoip@gmail.com>
2025-08-04 19:22:23 -0300

    Config: Sync extensions document

--- gitapps/src/Components/RecipeComponent/RecipeForm/index.tsx
+++ gitapps/src/Components/RecipeComponent/RecipeForm/index.tsx
@@ -1,85 +1,128 @@
-import { Button, Checkbox, FormLabel, Grid2, Slider, TextField, } from "@mui/material"
-import { Container, TopDecorativeBar, TopFormNav } from "./styles"
-import { useForm } from "react-hook-form"
-import { useState } from "react"
+import {
+  Button,
+  Checkbox,
+  FormLabel,
+  Grid2,
+  Slider,
+  TextField,
+} from "@mui/material";
+import { Container, TopDecorativeBar, TopFormNav } from "./styles";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
 
 interface IRecipe {
-    name: string;
-    prepareTime: string;
-    dificulty: string;
-    isVegan: boolean;
-    ingredients: string;
-    prepareSteps: string
-
+  name: string;
+  prepareTime: string;
+  dificulty: string;
+  isVegan: boolean;
+  ingredients: string;
+  prepareSteps: string;
 }
 
 function RecipeForm() {
+  const [newRecipe, setNewRecipe] = useState<IRecipe>({} as IRecipe);
 
-    const [newRecipe, setNewRecipe] = useState<IRecipe>({} as IRecipe)
+  const { register, handleSubmit } = useForm<IRecipe>();
 
+  const logRecipe = (d: IRecipe) => {
+    setNewRecipe(d);
+    console.log(newRecipe);
+  };
 
-    const { register, handleSubmit } = useForm<IRecipe>()
+  return (
+    <>
+      <Container>
+        <TopDecorativeBar>a</TopDecorativeBar>
+        <TopFormNav>
+          <p>Criar sua</p> <span> receita</span>
+        </TopFormNav>
+        <Grid2 container spacing={1} columns={{ xs: 1, md: 4 }}>
+          <Grid2 size={2} container>
+            <FormLabel>Nome da receita</FormLabel>
+            <TextField
+              fullWidth
+              variant="outlined"
+              size="small"
+              {...register("name", { required: true })}
+            />
+          </Grid2>
 
-    const logRecipe = (d: IRecipe) => {
+          <Grid2 size={{ xs: 1, md: 0.5 }} container>
+            <FormLabel>Preparo</FormLabel>
+            <TextField
+              type="text"
+              fullWidth
+              variant="outlined"
+              size="small"
+              {...register("prepareTime", { required: true })}
+            />
+          </Grid2>
 
-        setNewRecipe(d)
+          <Grid2 size={{ xs: 1, md: 1 }} container>
+            <FormLabel>Dificuldade</FormLabel>
+            <Slider
+              defaultValue={0}
+              min={0}
+              max={5}
+              step={1}
+              valueLabelDisplay="on"
+              {...register("dificulty")}
+            />
+          </Grid2>
 
-        console.log(newRecipe);
+          <Grid2 size={1} container>
+            <FormLabel>Vegano?</FormLabel>
+            <Checkbox
+              size="small"
+              {...(register("isVegan"), { required: false })}
+            />
+          </Grid2>
 
-    }
+          <Grid2 size={4} container>
+            <FormLabel>Ingredientes</FormLabel>
+            <TextField
+              multiline
+              type="text"
+              fullWidth
+              minRows={2}
+              maxRows={4}
+              size="medium"
+              {...register("ingredients")}
+            />
+          </Grid2>
 
+          <Grid2 size={4} container>
+            <FormLabel>Como preparar</FormLabel>
+            <TextField
+              multiline
+              type="text"
+              fullWidth
+              minRows={2}
+              maxRows={4}
+              size="medium"
+              {...register("prepareSteps")}
+            />
+          </Grid2>
 
-    return (
-        <>
-            <Container >
-                <TopDecorativeBar>a</TopDecorativeBar>
-                <TopFormNav><p>Criar sua</p> <span> receita</span></TopFormNav>
-                <Grid2 container spacing={1} columns={{ xs: 1, md: 4 }}  >
-
-                    <Grid2 size={2} container  >
-                        <FormLabel>Nome da receita</FormLabel>
-                        <TextField fullWidth variant="outlined" size="small" {...register('name', { required: true })} />
-                    </Grid2>
-
-                    <Grid2 size={{ xs: 1, md: 0.5 }} container  >
-                        <FormLabel >Preparo</FormLabel>
-                        <TextField type="text" fullWidth variant="outlined" size="small" {...register("prepareTime", { required: true })} />
-                    </Grid2>
-
-                    <Grid2 size={{ xs: 1, md: 1 }} container >
-                        <FormLabel>Dificuldade</FormLabel>
-                        <Slider defaultValue={0} min={0} max={5} step={1} valueLabelDisplay="on"  {...register("dificulty")} />
-                    </Grid2>
-
-                    <Grid2 size={1} container  >
-                        <FormLabel>Vegano?</FormLabel>
-                        <Checkbox size="small" {...register('isVegan', { required: true })} />
-                    </Grid2>
-
-                    <Grid2 size={4} container >
-                        <FormLabel >Ingredientes</FormLabel>
-                        <TextField multiline type="text" fullWidth minRows={2} maxRows={4} size="medium" {...register("ingredients")} />
-                    </Grid2>
-
-                    <Grid2 size={4} container >
-                        <FormLabel >Como preparar</FormLabel>
-                        <TextField multiline type="text" fullWidth minRows={2} maxRows={4} size="medium" {...register("prepareSteps")} />
-                    </Grid2>
-
-                    <Grid2 size={4} container >
-                        <Button
-                            style={{ fontFamily: "poppins", backgroundColor: "var(--form-yellow)", color: "var(--form-black)" }}
-                            fullWidth
-                            color="primary"
-                            variant="contained"
-                            onClick={() => handleSubmit(logRecipe)()}>Criar</Button>
-                    </Grid2>
-
-                </Grid2>
-            </Container>
-        </>
-    )
-
+          <Grid2 size={4} container>
+            <Button
+              style={{
+                fontFamily: "poppins",
+                backgroundColor: "var(--form-yellow)",
+                color: "var(--form-black)",
+              }}
+              fullWidth
+              color="primary"
+              variant="contained"
+              onClick={() => handleSubmit(logRecipe)()}
+            >
+              Criar
+            </Button>
+          </Grid2>
+        </Grid2>
+      </Container>
+    </>
+  );
 }
 
-export default RecipeForm
+export default RecipeForm;
